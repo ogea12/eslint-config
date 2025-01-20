d833d361b5d9e85a2ca8c31ed341bc846f01d89e
Julien Benac <julien.benac@protonmail.com>
2025-01-20 22:36:36 +0100

    feat: add custom callees to tailwindcss shared settings

--- src/configs/tailwindcss.ts
+++ src/configs/tailwindcss.ts
@@ -5,5 +5,8 @@ import flattenArray from '../lib/flatten_array.js'
 /** ESLint configuration object for Tailwind CSS's rules. */
 const config = flattenArray(pluginTailwindCSS.configs['flat/recommended'])
 config.name = 'Tailwind CSS' // Override the configuration name
+config.settings = {
+  tailwindcss: { callees: ['classnames', 'clsx', 'ctl', 'cva', 'tv', 'twMerge'] },
+}
 
 export default config
